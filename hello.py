8c920377ba229adf34ad0094f64939977bf7d8e0
RuiGene <danielyangnz@hotmail.com>
2023-03-01 14:42:14 +1300

    rearragned docs

--- hello.py
+++ hello.py
@@ -1,137 +1,153 @@
+# https://site.financialmodelingprep.com/developer/docs/
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
 import fundamentalanalysis as fa
-from datetime import datetime
-FA_API_KEY = '85fe259a4ec6fad3cbe55a5ddaf7f9b4'
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+from datetime import date, timedelta, datetime
+API_KEY = '85fe259a4ec6fad3cbe55a5ddaf7f9b4'
 
-st.set_page_config(page_title="Stock searcher", layout="wide")
-ticker = st.columns(4)[0].text_input("Ticker")
+st.set_page_config(page_title="Stocks", layout = "wide")
+col1, col2, col3, col4  = st.columns([2, 0.1, 0.1, 0.5])
+with col4:
+    ticker = st.text_input("Ticker", help = "Enter the stock ticker here").upper() # Search bar for stock ticker
 
-TIME_DIFFS = {
-    "1 week": pd.DateOffset(weeks=1),
-    "1 month": pd.DateOffset(months=1),
-    "3 months": pd.DateOffset(months=3),
-    "1 year": pd.DateOffset(years=1),
-    "3 years": pd.DateOffset(years=3),
-    "5 years": pd.DateOffset(years=5)
-}
-
-
-# save for 3 hours
-@st.cache(ttl=60*60*3)
-def load_data(ticker):
-    # load data
-    profile = fa.profile(ticker, FA_API_KEY)
-    key_metrics_annually = fa.key_metrics(ticker, FA_API_KEY, period="annual")
-    stock_data = fa.stock_data(ticker, period="5y", interval="1d")
-    financial_ratios_annually = fa.financial_ratios(ticker, FA_API_KEY, period="annual")
-    income_statement_annually = fa.income_statement(ticker, FA_API_KEY, period="annual")
-    try:
-        dividends = fa.stock_dividend(ticker, FA_API_KEY)
-        dividends.index = pd.to_datetime(dividends.index)
-        dividends = dividends["adjDividend"].resample("1Y").sum().sort_index()
-    except:
-        dividends = pd.Series(0, name="Dividends")
-
-    # return information of interest
+# Retrieving information via API
+def data(stock_ticker):
+    profile = fa.profile(ticker, API_KEY)
+    prices = fa.stock_data(ticker, period = "5y", interval = "1d")
+    key_metrics_annually = fa.key_metrics(ticker, API_KEY, period = 'annual')
+    dividends = fa.stock_dividend(ticker, API_KEY)
     return {
-        "stock_closings": stock_data["close"].sort_index(),
-        "historical_PE": key_metrics_annually.loc["peRatio"].sort_index(),
-        "payout_ratio": financial_ratios_annually.loc["payoutRatio"].sort_index(),
-        "dividend_yield": 100*financial_ratios_annually.loc["dividendYield"].sort_index(),
-        "cash_per_share": key_metrics_annually.loc["cashPerShare"].sort_index(),
-        "debt_to_equity": key_metrics_annually.loc["debtToEquity"].sort_index(),
-        "free_cash_flow_per_share": key_metrics_annually.loc["freeCashFlowPerShare"].sort_index(),
-        "dividends": dividends,
-        "earnings_per_share": income_statement_annually.loc["eps"].sort_index(),
-        "info": profile.iloc[:, 0]
+        "company_info": profile.iloc[:, 0],
+        "close_price": prices["close"].sort_index(),
+        "key_metrics": key_metrics_annually.iloc[:, 0],
+        "dividends": dividends
     }
 
 if ticker != "":
-    stock_data = load_data(ticker)
+    stock_data = data(ticker)
 
-info = stock_data["info"]
-currency = info["currency"]
+# Stock ticker title
+with col1:
+    st.title("{} ({})".format(stock_data["company_info"]["companyName"], stock_data["company_info"]["symbol"]))
 
-# Add changes for different periods
-close = stock_data["stock_closings"]
-latest_price = close.iloc[-1]
-# should all be displayed on the same row
-change_columns = st.columns(len(TIME_DIFFS))
-today = pd.to_datetime("today").floor("D")
-for i, (name, difference) in enumerate(TIME_DIFFS.items()):
-    # go back to the date <difference> ago
-    date = (today - difference)
-    # if there is no data back then, then use the earliest
-    if date < close.index[0]:
-        date = close.index[0]
-    # if no match, get the date closest to it back in time, e.g. weekend to friday
-    previous_price = close.iloc[close.index.get_loc(date,method='ffill')]
-    # calculate change in percent
-    change = 100*(latest_price - previous_price) / previous_price
-    # show red if negative, green if positive
-    color = "red" if change < 0 else "green"
+st.markdown(stock_data["company_info"]["description"]) # Adding description section
+# title_text = f"{stock_data['company_info']['companyName']} price"
+st.markdown("""
+    <style>
+    .stRadio [role=radiogroup]{
+        align-items: center;
+        justify-content: center;
+    }
+    </style>
+""",unsafe_allow_html=True)
+options = ['1 Week', '1 Month', '3 Months', '6 Months', '1 Year', '3 Years', '5 Years']
+time = st.radio(label = ' ', options = options, horizontal = True)
 
-    # color can be displayed as :red[this will be red] in markdown
-    change_columns[i].markdown(f"{name}: :{color}[{round(change, 2)}%]")
+def convert_date(date_string):
+    return datetime.strptime(date_string, "%Y-%m-%d").date()
+if stock_data["dividends"].empty == False:
+    dividend_dates = stock_data["dividends"]["paymentDate"].values
+    dividend_dates = dividend_dates[dividend_dates != ""]   
+    dividend_dates = np.vectorize(convert_date)(dividend_dates)
 
-overview_columns = st.columns([1, 3])
+def graph(closing, time_frame):
+    today = date.today()
+    if time_frame == '1 Week':
+        start_date = today - timedelta(days = 7)
+    elif time_frame == '1 Month':
+        start_date = today - timedelta(days = 30)
+    elif time_frame == '3 Months':
+        start_date = today - timedelta(days = 90)
+    elif time_frame == '6 Months':
+        start_date = today - timedelta(days = 180)
+    elif time_frame == '1 Year':
+        start_date = today - timedelta(days = 365)
+    elif time_frame == '3 Years':
+        start_date = today - timedelta(days = 1095)
+    elif time_frame == '5 Years':
+        start_date = today - timedelta(days = 1825)
+    else:
+        raise ValueError("Invalid time frame")
+    fig = px.line(x= closing[start_date:today].index, y = closing.loc[start_date:today])
+    fig.update_traces(line_color = "blue", name = "Price", showlegend = True)
+ 
+    # Add crosses for the dividend payout dates
+    if stock_data["dividends"].empty == False:
+        past_dates = dividend_dates[dividend_dates > start_date]
+        dividend_trace = go.Scatter(
+            x = past_dates,
+            y = closing.loc[past_dates],
+            mode = 'markers',
+            marker = dict(
+                symbol = 'circle',
+                size = 7,
+                color = 'black',
+                line = dict(width = 1, color = 'black')
+            ),
+            name = 'Dividend Payouts'
+        )
+        fig.add_trace(dividend_trace)
+        fig.update_layout(
+            yaxis_title = "Price ($)",
+            xaxis_title = "Date",
+            showlegend = True
+        )
+        return fig
+    else:
+        fig.update_layout(
+        yaxis_title = "Price ($)",
+        xaxis_title = "Date",
+        showlegend = True
+        )
+        return fig
 
-overview_columns[0].markdown("<br/>"*4, unsafe_allow_html=True)
-# text will be displayed and key is the key in info
-for text, key in [
-    ("Current price", "price"),
-    ("Country", "country"),
-    ("Exchange", "exchange"),
-    ("Sector", "sector"),
-    ("Industry", "industry"),
-    ("Full time employees", "fullTimeEmployees")
-]:
-    overview_columns[0].markdown("")
-    overview_columns[0].markdown(f"- {text}: **{info[key]}**")
+closing = stock_data["close_price"]
+fig = graph(closing, time)
+st.plotly_chart(fig, use_container_width=True)
 
-graph_placeholder = overview_columns[1].empty()
-graph_placeholder.plotly_chart(go.Figure(), use_container_width=True)
+# Define the pre-defined values
+price = '$' + str(stock_data["company_info"]["price"]) + ' ' + stock_data["company_info"]["currency"]
+volume = str(f'{stock_data["company_info"]["volAvg"]:,}')
+mcap = '$' + str(f'{round(stock_data["company_info"]["mktCap"]/1000000, 2):,}')  +  'M'
+sector = stock_data["company_info"]["sector"]
+country = stock_data["company_info"]["country"]
+exchange = stock_data["company_info"]["exchangeShortName"]
+industry = stock_data["company_info"]["industry"]
+pe = str(round(stock_data["key_metrics"]["peRatio"], 2))
+pb = str(round(stock_data["key_metrics"]["pbRatio"], 2))
+eps = str(round(stock_data["key_metrics"]["netIncomePerShare"], 2))
+ev_ebitda = str(round(stock_data["key_metrics"]["enterpriseValueOverEBITDA"], 2))
+ev_revenue = str(round(stock_data["key_metrics"]["evToSales"], 2))
+if stock_data['key_metrics']["dividendYield"] == None:
+    dividend_yield = 'None'
+else:
+    dividend_yield = str(round(stock_data["key_metrics"]["dividendYield"], 4) * 100) + '%'
+debt_to_equity = str(round(stock_data["key_metrics"]["debtToEquity"], 2))
 
-# options that will dictate the graph:
+table = {
+    'Company Summary': ['Price', 'Volume', 'Market Cap', 'Sector', 'Country', 'Exchange', 'Industry'],
+    'Metrics:': [price, volume, mcap, sector, country, exchange, industry],
+    'Comparable Metrics': ['P/E', 'P/B', 'EPS', 'EV/EBITDA', 'EV/Revenue', 'Dividend yield', 'Debt/Equity'],
+    'Values:': [pe, pb, eps, ev_ebitda, ev_revenue, dividend_yield, debt_to_equity]      
+}
+df2 = pd.DataFrame(table)
+hide_table_row_index = """
+            <style>
+            thead tr th:first-child {display:none}
+            tbody th {display:none}
+            </style>
+            """
 
-# radio buttons for what time window to display the stock price
-time_window_key = overview_columns[1].radio("Time window", TIME_DIFFS.keys(), index=len(TIME_DIFFS)-1, horizontal=True)
-# select the value from the key, i.e. the pd.DateOffset
-time_window = TIME_DIFFS[time_window_key]
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+# Display a static table
+st.table(df2)
 
-# slider to select the moving average to display in the graph
-moving_average = overview_columns[1].slider("Moving average", min_value=2, max_value=500, value=30)
-
-# Use above to construct the graph:
-
-def get_price_data_fig(srs, moving_average, time_window, currency):
-    # create moving average
-    ma = srs.rolling(window=moving_average).mean().dropna()
-    # only in time window
-    start = (pd.to_datetime("today").floor("D") - time_window)
-    srs = srs.loc[start:]
-    ma = ma.loc[start:]
-    # create figures for normal and moving average
-    fig1 = px.line(y=srs, x=srs.index)
-    fig1.update_traces(line_color="blue", name="Price", showlegend=True)
-    fig2 = px.line(y=ma, x=ma.index)
-    fig2.update_traces(line_color="orange", name=f"Moving average price ({moving_average})", showlegend=True)
-    # combine and add layout
-    fig = go.Figure(data = fig1.data + fig2.data)
-    fig.update_layout(
-        xaxis_title="Date",
-        yaxis_title=currency,
-        title_x = 0.5,
-        # align labels top-left, side-by-side
-        legend=dict(y=1.1, x=0, orientation="h"),
-        showlegend=True
-    )
-    return fig
-
-# show the graph
-fig = get_price_data_fig(stock_data["stock_closings"], moving_average, time_window, time_window_key, currency)
-# add to placeholder to be displayed before options
-graph_placeholder.plotly_chart(fig, use_container_width=True)
+if stock_data["dividends"].empty == False:
+    payout = stock_data["dividends"]["adjDividend"][0:5]
+    fig = go.Figure(go.Bar(x = payout.index, y = payout))
+    fig.update_layout(xaxis_title = 'Date', yaxis_title = 'Payout ($)', title = {"text": "Dividend History", "x": 0.5, "xanchor": 'center'})
+    st.plotly_chart(fig, use_container_width = True)
