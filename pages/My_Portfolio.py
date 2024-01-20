f35c38c0fbefa9a1f7b7c19894d8d54cd868e9a8
RuiGene <zrdanielyang@gmail.com>
2024-01-20 15:25:47 +1300

    Updated data table

--- pages/My_Portfolio.py
+++ pages/My_Portfolio.py
@@ -25,8 +25,8 @@ def portfolio(tickers, shares, date_purchased):
     for i in range(len(tickers)):
         stock = yf.Ticker(tickers[i])
         stock_data = stock.history(start = date_purchased[i])
-        stock_data['portfolio_value'] = stock_data['Close'] * shares[i]
-        data = pd.concat([data, stock_data['portfolio_value']], axis = 1)
+        stock_data[tickers[i]] = stock_data['Close'] * shares[i]
+        data = pd.concat([data, stock_data[tickers[i]]], axis = 1)
 
     data['total_portfolio_value'] = data.sum(axis=1)
     fig, ax = plt.subplots(figsize=(10, 5)) # set the figsize parameter to increase the width of the plot
