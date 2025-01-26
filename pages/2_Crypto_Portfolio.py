b1e7c98b5bda129f3bf4b5b7477e8ed3b147e457
RuiGene <zrdanielyang@gmail.com>
2025-01-26 21:16:16 +1300

    Added new page and set up page configs

---
+++ pages/2_Crypto_Portfolio.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.set_page_config(page_title="Crypto Portfolio", layout = "wide", page_icon = '🪙')
---
+++ pages/5_Watchlist.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.set_page_config(page_title="Watchlist", layout = "wide", page_icon = '👀')
